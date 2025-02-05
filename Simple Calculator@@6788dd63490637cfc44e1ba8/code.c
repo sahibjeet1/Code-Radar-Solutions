27d4d597cb4a47e5cfd8b808f632dc880c026d05
Sahibjeet Singh <196170874+sahibjeet1@users.noreply.github.com>
2025-02-05 23:13:08 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int a,b, result;
+    int a,b,result;
     char opr;
     scanf("%d %d %c", &a, &b, &opr);
     
@@ -15,7 +15,7 @@ int main() {
         printf("%d");
     } else if (opr=='/')
     {
-        if(b!=2){
+        if(b!=0){
             result=a/b;
             printf("%d", a/b);
             {else 
@@ -26,7 +26,6 @@ int main() {
         printf("error");
     }
     }
-    
     printf("%d", &result);
 
     return 0;
